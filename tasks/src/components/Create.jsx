633da6df8506999ff9a6a8c2d064fc6e936e1fc7
Petermwansa <mwansapeter800@gmail.com>
2025-04-22 19:02:38 +0300

    Add the project to the reposistory with dependencies installed

---
+++ tasks/src/components/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Create = () => {
+  return (
+    <div>
+      <input type='text' name='' id='' />
+      <button type='submit'>Add Tasks</button>
+    </div>
+  )
+}
+
+export default Create
---
+++ tasks/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Create from './Create'
+
+const Home = () => {
+
+    
+
+  return (
+    <div>
+      <h2>Tasks</h2>
+      <Create />
+    </div>
+  )
+}
+
+export default Home
